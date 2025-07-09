0423e219f85ea3f043501cf08309aa1c8e8696de
julietwainoi <juliewainoi@gmail.com>
2025-07-09 08:49:46 +0300

    removed the oidc creation

--- main.tf
+++ main.tf
@@ -1,37 +1,6 @@
 provider "aws" {
   region = "us-west-2"
 }
-
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub's cert
-}
-
-resource "aws_iam_role" "github_oidc_role" {
-  name = "GitHubOIDCDeploymentRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.github.arn
-      },
-      Action = "sts:AssumeRoleWithWebIdentity",
-      Condition = {
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:julietwainoi/ServerlessRESTAPIwithAWS-Lambda-_APIGateway-_DynamoDB:ref:refs/heads/main"
-        }
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_admin" {
-  role       = aws_iam_role.github_oidc_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" # Or something more restrictive
-}
-
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
 
