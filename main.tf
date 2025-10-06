c7299eac23e43937e5b9f92a1aaa9288e849879e
julietwainoi <juliewainoi@gmail.com>
2025-10-06 15:16:41 +0300

    added authorization on the aws _api_gateway method

--- main.tf
+++ main.tf
@@ -100,13 +100,15 @@ resource "aws_api_gateway_method" "get_item" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.item.id
   http_method   = "GET"
-  authorization = "NONE"
+  //authorization = "NONE"
+    authorization = "AWS_IAM"
 }
 resource "aws_api_gateway_method" "post_item" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.items.id
   http_method   = "POST"
-  authorization = "NONE"
+  //authorization = "NONE"
+    authorization = "AWS_IAM"
 }
 resource "aws_api_gateway_integration" "lambda_post_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
