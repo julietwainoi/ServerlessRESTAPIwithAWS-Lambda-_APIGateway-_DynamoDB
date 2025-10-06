b4a92ed25ae9697986590bbc7fd7dd468a1bd03b
julietwainoi <juliewainoi@gmail.com>
2025-10-06 12:21:56 +0300

    created a new s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "juliet-terraform-state-bucket-2025"
+    bucket         = "juliet-terraform-state-bucket-202510"
     key            = "dev/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-locks" # optional but recommended
